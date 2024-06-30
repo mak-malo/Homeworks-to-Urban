796afc6f81f459fdaedd784da7b00abf2f8f70f0
Malyuchenko Maxim <163737524+mak-malo@users.noreply.github.com>
2024-06-30 12:34:23 +0300

    Delete homework_3.py

--- homework_3.py
+++
@@ -1,9 +0,0 @@
-#homework_3
-name = 'Maxim'
-print('Имя:', name)
-age = 23
-print('Возраст:', age)
-age = age + 27
-print('Обновленная переменная возраста', age)
-is_student = True
-print('Студент?', is_student)
