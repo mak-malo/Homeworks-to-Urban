2753afd27dcbd1bdefcf250057642d76c8ec04a3
Malyuchenko Maxim <163737524+mak-malo@users.noreply.github.com>
2024-06-30 12:34:16 +0300

    Delete homework_2.py

--- homework_2.py
+++
@@ -1,7 +0,0 @@
-#homework_2
-homeworks = 12
-work_hours = 1.5
-course = 'Python'
-time_to_homework = work_hours / homeworks
-print(f'{course}, всего задач: {homeworks}, затрачено часов: {work_hours}, среднее время выполнения: {time_to_homework} часа')
-print(course, ' , всего задач: ', homeworks, ' затрачено часов: ', work_hours, ' среднее время выполнения: ', time_to_homework, ' часа ', sep = '')
