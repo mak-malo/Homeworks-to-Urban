48f9dbf3e8096dcaf7879d6624b7cd7ae3cbb1d4
Malyuchenko Maxim <163737524+mak-malo@users.noreply.github.com>
2024-07-28 02:12:17 +0300

    Delete module_2_3.py

--- module_2_3.py
+++
@@ -1,8 +0,0 @@
-my_list = [42, 69, 0, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5] # Для проверки кода меняйте исходные данные
-index_my_list = 0
-while index_my_list < len(my_list):
-    if my_list[index_my_list] > 0:
-        print(my_list[index_my_list])
-    elif my_list[index_my_list] < 0:
-        break
-    index_my_list += 1
