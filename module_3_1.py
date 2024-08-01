95302d3d1b4beecc329acd3244605ec0011f3a26
Malyuchenko Maxim <163737524+mak-malo@users.noreply.github.com>
2024-08-02 01:23:27 +0400

    Update module_3_1.py
    Исправления по комментарию:
    
    "Код хороший, но стоит еще раз поработать с функцией count_calls. Ваш вариант не совсем оптимально работает. Вместо того, чтобы в ручную менять значение count_calls_function, попробуйте написать функцию, которая при вызове уже сама меняла значение на +1."

--- module_3_1.py
+++ module_3_1.py
@@ -1,19 +1,30 @@
 count_calls_functions = 0
+#________________________________________________________________________________________
+def count_calls_up ():
+    global coucount_calls_functions
+    count_calls_functions = count_calls_functions + 2 
+    # Раз для увечиления числа на счетчике вызова функций 
+    # я вызываю функцию, то прибавляю сразу 2 - за основную функцию и за count_calls_up
+#________________________________________________________________________________________
+
 
 def count_calls():
     global count_calls_functions
-    calls = count_calls_functions
+    calls = count_calls_functions 
+    # переадресация переменной здесь для того, чтобы при вызове этой функции счетчик вызовов
+    # учитывал сам вызов этой функции, но при этом возвращал значение счетчика до вызова count_calls
+#_____________________________________
+    count_calls_up()
+#_____________________________________
     return calls
 
 def string_info(work_str):
-    global count_calls_functions
-    count_calls_functions += 1
+    count_calls_up()
     info_tuple = len(work_str), work_str.upper(), work_str.lower()
     return info_tuple
 
 def is_contains(find_str, main_list):
-    global count_calls_functions
-    count_calls_functions += 1
+    count_calls_up()
     for i in main_list:
         if i.lower() == find_str.lower():
             return True
